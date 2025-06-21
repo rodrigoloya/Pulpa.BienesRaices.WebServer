6d0cf08afd243fc762356922b06fa89b77223ef5
rodrigoloya <al_corp@hotmail.com>
2025-06-21 00:29:45 -0400

    Add Tailwind CSS and new scrollToBottom function
    - Imported Tailwind CSS in `input.css` for enhanced styling.
    - Introduced `scrollToBotton` function in `chat.js` for smooth scrolling to the bottom of an element with error handling.
    - Updated `tailwind.css` with a comprehensive set of utility classes, properties, and themes for improved design flexibility.

---
+++ Pulpa.BienesRaices.WebServer/wwwroot/chat.js
@@ -0,0 +1,12 @@
+﻿export function scrollToBotton(element) {
+    try {
+        if (element) {
+            element.scrollTo({
+                top: element.scrollHeight,
+                behavior: 'smooth'
+            });
+        }
+    } catch (e) {
+        console.error('Error scrolling to bottom:', e);
+    }
+}
